227692127c872a4c75cdd96b58861ececba07afc
lanang <lananglanusaputera@gmail.com>
2024-04-25 12:44:59 +0800

    lanang

--- src/App.jsx
+++ src/App.jsx
@@ -20,21 +20,57 @@ function App() {
 
   const [zoom, setZoom] = useState(100);
   const contentRef = useRef(null);
-  const bookRef = useRef(null);
 
   useEffect(() => {
     contentRef.current.style.zoom = `${zoom}%`;
-    // document.body.style.zoom = `${zoom}%`;
 
-    if (zoom > 100) {
-      const backdrop = document.querySelector('.backdrop');
-      const contentWidth = bookRef.current.innerWidth;
-      const contentHeight = bookRef.current.innerHeight;
-      backdrop.style.width = `${contentWidth}px`;
-      backdrop.style.height = `${contentHeight}px`;
+    // Tambahkan event listener ke elemen konten untuk menanggapi keyboard input
+    const content = contentRef.current;
+    content.addEventListener('keydown', handleKeyDown);
+
+    // Hapus event listener saat komponen dibongkar
+    return () => {
+      content.removeEventListener('keydown', handleKeyDown);
+    };
+
+  }, [zoom, contentRef]);
+
+  const handleKeyDown = (event) => {
+    const content = contentRef.current;
+    if (content) {
+      switch (event.key) {
+        case 'ArrowUp':
+          content.scrollTop -= 50; // Sesuaikan jumlah scroll
+          break;
+        case 'ArrowDown':
+          content.scrollTop += 50; // Sesuaikan jumlah scroll
+          break;
+        case 'ArrowLeft':
+          content.scrollLeft -= 50; // Sesuaikan jumlah scroll
+          break;
+        case 'ArrowRight':
+          content.scrollLeft += 50; // Sesuaikan jumlah scroll
+          break;
+        default:
+          break;
+      }
     }
+  };
 
-  }, [zoom, contentRef, bookRef]);
+  const scrollContent = (direction) => {
+    const content = contentRef.current;
+    if (content) {
+      if (direction === 'up') {
+        content.scrollTop -= 50; // Sesuaikan jumlah scroll
+      } else if (direction === 'down') {
+        content.scrollTop += 50; // Sesuaikan jumlah scroll
+      } else if (direction === 'left') {
+        content.scrollLeft -= 50; // Sesuaikan jumlah scroll
+      } else if (direction === 'right') {
+        content.scrollLeft += 50; // Sesuaikan jumlah scroll
+      }
+    }
+  };
 
   return (
     <div className='overflow-x-hidden overflow-y-auto h-screen bg-gradient-to-r from-blue-950 to-blue-700 flex justify-center lg:items-center'>
@@ -57,45 +93,53 @@ function App() {
               <p className="hidden lg:block mt-8 text-sm text-zinc-300">Copyright © Alfa Prima {new Date().getFullYear()}</p>
             </div>
           </div>
-          <div ref={contentRef} className={`lg:basis-2/3 pb-4 lg:pb-0 relative ${zoom > 100 ? "overflow-scroll" : "overflow-hidden"}`}>
-            {zoom > 100 && <div className="backdrop absolute z-50 bg-red-500/50"></div>}
-            <HTMLFlipBook
-                ref={bookRef}
-                width={450}
-                height={600}
-                size="stretch"
-                minWidth={280}
-                maxWidth={500}
-                minHeight={380}
-                maxHeight={800}
-                maxShadowOpacity={0.5}
-                showCover={false}
-                mobileScrollSupport={false}
-                // autoSize={true}
-                // disableFlipByClick
-                // clickEventForward={false}
-            >
-              <Page nextButton={false} number="1" src="/img/1.JPG" alt="Brosur_Halaman_1"/>
-              <Page nextButton number="2" src="/img/2.JPG" alt="Brosur_Halaman_2"/>
-              <Page nextButton number="3" src="/img/3.JPG" alt="Brosur_Halaman_3"/>
-              <Page nextButton number="4" src="/img/4.JPG" alt="Brosur_Halaman_4"/>
-              <Page nextButton number="5" src="/img/5.JPG" alt="Brosur_Halaman_5"/>
-              <Page nextButton number="6" src="/img/6.JPG" alt="Brosur_Halaman_6"/>
-              <Page nextButton number="7" src="/img/7.JPG" alt="Brosur_Halaman_7"/>
-              <Page nextButton number="8" src="/img/8.JPG" alt="Brosur_Halaman_8"/>
-              <Page nextButton number="9" src="/img/9.JPG" alt="Brosur_Halaman_9"/>
-              <Page nextButton number="10" src="/img/10.JPG" alt="Brosur_Halaman_10"/>
-              <Page nextButton number="11" src="/img/11.JPG" alt="Brosur_Halaman_11"/>
-              <Page nextButton={false} number="12" src="/img/12.JPG" alt="Brosur_Halaman_12"/>
-            </HTMLFlipBook>
+          <div tabIndex={0} ref={contentRef} className={`lg:basis-2/3 pb-4 lg:pb-0 relative ${zoom > 100 ? "overflow-scroll aspect-[2/3] md:aspect-auto" : "overflow-hidden"}`}>
+            <div className="relative">
+              {/* {zoom > 100 && <div className="absolute w-full h-full z-50 bg-red-500/50"/>} */}
+              <HTMLFlipBook
+                  width={450}
+                  height={600}
+                  size="stretch"
+                  minWidth={280}
+                  maxWidth={500}
+                  minHeight={380}
+                  maxHeight={800}
+                  maxShadowOpacity={0.5}
+                  showCover={false}
+                  mobileScrollSupport={true}
+                  // autoSize={true}
+                  // disableFlipByClick
+                  // clickEventForward
+                  onFlip={false}
+                  useMouseEvents
+              >
+                <Page nextButton={false} number="1" src="/img/1.JPG" alt="Brosur_Halaman_1"/>
+                <Page nextButton number="2" src="/img/2.JPG" alt="Brosur_Halaman_2"/>
+                <Page nextButton number="3" src="/img/3.JPG" alt="Brosur_Halaman_3"/>
+                <Page nextButton number="4" src="/img/4.JPG" alt="Brosur_Halaman_4"/>
+                <Page nextButton number="5" src="/img/5.JPG" alt="Brosur_Halaman_5"/>
+                <Page nextButton number="6" src="/img/6.JPG" alt="Brosur_Halaman_6"/>
+                <Page nextButton number="7" src="/img/7.JPG" alt="Brosur_Halaman_7"/>
+                <Page nextButton number="8" src="/img/8.JPG" alt="Brosur_Halaman_8"/>
+                <Page nextButton number="9" src="/img/9.JPG" alt="Brosur_Halaman_9"/>
+                <Page nextButton number="10" src="/img/10.JPG" alt="Brosur_Halaman_10"/>
+                <Page nextButton number="11" src="/img/11.JPG" alt="Brosur_Halaman_11"/>
+                <Page nextButton={false} number="12" src="/img/12.JPG" alt="Brosur_Halaman_12"/>
+              </HTMLFlipBook>
+            </div>
           </div>
           <div className="block lg:hidden">
             <p className="py-4 text-center text-sm text-zinc-300">Copyright © Alfa Prima {new Date().getFullYear()}</p>
           </div>
-          <div className="fixed start-0 bottom-0">
-            <button onClick={() => setZoom(zoom + 15)} type="button" className="p-2 bg-red-500">{"+"}</button>
-            <button onClick={() => setZoom(zoom - 15)} type="button" className="p-2 bg-green-500">{"-"}</button>
-            <button onClick={() => setZoom(100)} type="button" className="p-2 bg-yellow-500">{"reset"}</button>
+          <div className="scale-75 md:scale-90 fixed start-1/2 -translate-x-1/2 bottom-0 flex items-center bg-zinc-900 rounded-lg p-2">
+            <button onClick={() => setZoom(zoom + 15)} disabled={zoom < 190 ? false : true} type="button" className={`${zoom < 190 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 transition duration-200 mx-1`}><i className="fa fa-fw fa-plus"/> Zoom In</button>
+            <button onClick={() => setZoom(zoom - 15)} disabled={zoom > 100 ? false : true} type="button" className={`${zoom > 100 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 transition duration-200 mx-1`}><i className="fa fa-fw fa-minus"/> Zoom Out</button>
+            <button onClick={() => setZoom(100)} disabled={zoom > 100 ? false : true} type="button" className={`${zoom > 100 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 hover:bg-zinc-700/75 transition duration-200 mx-1`}><i className="fa fa-fw fa-rotate-right"/> Reset</button>
+
+            <button onClick={() => scrollContent("left")} disabled={zoom > 100 ? false : true} type="button" className={`${zoom > 100 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 hover:bg-zinc-700/75 transition duration-200 mx-1`}><i className="fa fa-fw fa-left-long"/></button>
+            <button onClick={() => scrollContent("up")} disabled={zoom > 100 ? false : true} type="button" className={`${zoom > 100 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 hover:bg-zinc-700/75 transition duration-200 mx-1`}><i className="fa fa-fw fa-up-long"/></button>
+            <button onClick={() => scrollContent("down")} disabled={zoom > 100 ? false : true} type="button" className={`${zoom > 100 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 hover:bg-zinc-700/75 transition duration-200 mx-1`}><i className="fa fa-fw fa-down-long"/></button>
+            <button onClick={() => scrollContent("right")} disabled={zoom > 100 ? false : true} type="button" className={`${zoom > 100 ? 'hover:bg-zinc-700/75' : 'opacity-50'} px-3 py-2 rounded text-nowrap text-xs font-medium text-white bg-zinc-800 hover:bg-zinc-700/75 transition duration-200 mx-1`}><i className="fa fa-fw fa-right-long"/></button>
           </div>
         </div>
       </div>
